ee205776670e47d4a7a241d4be7f3d93999eb265
Andrey Smelter <andrey.smelter@gmail.com>
2018-12-20 01:36:50 -0500

    Tests: new params.

--- tests/tests_cli.py
+++ tests/tests_cli.py
@@ -1,7 +1,11 @@
 import subprocess
+import pytest
 
 
-def test_version_cmd(impl):
-    cmd = ['python', '{}/naval_fate.py'.format(impl), '--version']
+@pytest.mark.parametrize('interpreter, interpreter_option, cli_option', [
+    ('python', '-m', '--version')
+])
+def test_version_cmd(impl, interpreter, interpreter_option, cli_option):
+    cmd = [interpreter, interpreter_option, impl, cli_option]
     result = subprocess.check_output(cmd)
     assert result.decode('utf-8').strip() == '1.0.0'
