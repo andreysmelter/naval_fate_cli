05dab4457535e3dff9fde3dfc5f9fd274d0ab0ac
Andrey Smelter <andrey.smelter@gmail.com>
2018-12-20 01:30:04 -0500

    Relative imports.

---
+++ docopt_cli/api.py
@@ -0,0 +1,24 @@
+def ship_new(names):
+    """Creates a new ship."""
+    for name in names:
+        print('Created ship {}'.format(name))
+
+
+def ship_move(ship, x, y, speed):
+    """Moves SHIP to the new location X,Y."""
+    print('Moving ship {} to [{},{}] with speed {} KN'.format(ship, x, y, speed))
+
+
+def ship_shoot(ship, x, y):
+    """Makes SHIP fire to X,Y."""
+    print('Ship {} fires to [{},{}]'.format(ship, x, y))
+
+
+def mine_set(x, y, ty):
+    """Sets a mine at a specific coordinate."""
+    print('Set {} mine at [{},{}]'.format(ty, x, y))
+
+
+def mine_remove(x, y):
+    """Removes a mine at a specific coordinate."""
+    print('Removed mine at [{},{}]'.format(x, y))
