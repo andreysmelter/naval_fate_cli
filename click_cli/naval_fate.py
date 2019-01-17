a976563d87741ec846a594f581f5f01bee51cd84
Andrey Smelter <andrey.smelter@gmail.com>
2019-01-17 15:31:56 -0500

    Reuse version from init.

--- click_cli/naval_fate.py
+++ click_cli/naval_fate.py
@@ -1,8 +1,10 @@
 import click
 
+from . import __version__
+
 
 @click.group()
-@click.version_option(version='1.0.0', message='%(version)s')
+@click.version_option(version=__version__, message='%(version)s')
 def cli():
     """Naval Fate."""
 
