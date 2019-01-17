5f0eed35c25c2c51435fe75db2e23b084c1dfb95
Andrey Smelter <andrey.smelter@gmail.com>
2019-01-17 14:57:25 -0500

    Tests: updated version test.

--- tests/tests_cli.py
+++ tests/tests_cli.py
@@ -5,7 +5,14 @@ import pytest
 @pytest.mark.parametrize('interpreter, interpreter_option, cli_option', [
     ('python', '-m', '--version')
 ])
-def test_version_cmd(impl, interpreter, interpreter_option, cli_option):
-    cmd = [interpreter, interpreter_option, impl, cli_option]
+def test_version_cmd(impl, interpreter, interpreter_options, cli_options):
+    cmd = [interpreter, interpreter_options, impl, cli_options]
     result = subprocess.check_output(cmd)
     assert result.decode('utf-8').strip() == '1.0.0'
+
+
+# @pytest.mark.parametrize('interpreter, cli, option', [
+#     ('python', 'naval_fate.py', '--version')
+# ])
+# def test_ship_new_cmd(impl, interpreter, cli, option):
+#    pass
