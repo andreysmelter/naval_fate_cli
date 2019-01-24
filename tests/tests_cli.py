ad370f8265354682accf3c7cee8e4777272dcc9c
Andrey Smelter <andrey.smelter@gmail.com>
2019-01-24 16:07:12 -0500

    Tests: added tests for "ship new" command.

--- tests/tests_cli.py
+++ tests/tests_cli.py
@@ -11,8 +11,13 @@ def test_version_cmd(impl, interpreter, interpreter_options, cli_options):
     assert result.decode('utf-8').strip() == '1.0.0'
 
 
-# @pytest.mark.parametrize('interpreter, cli, option', [
-#     ('python', 'naval_fate.py', '--version')
-# ])
-# def test_ship_new_cmd(impl, interpreter, cli, option):
-#    pass
+@pytest.mark.parametrize('interpreter, interpreter_options, cli_options', [
+    ('python', '-m', 'ship new Guardian'),
+    ('python', '-m', 'ship new Guardian1 Guardian2')
+])
+def test_ship_new_cmd(impl, interpreter, interpreter_options, cli_options):
+   cmd = [interpreter, interpreter_options, impl]
+   cmd.extend(cli_options.split())
+   result = subprocess.check_output(cmd, shell=False, stderr=subprocess.STDOUT)
+   list_of_ships = [word for word in result.decode('utf-8').split() if word.startswith('Guardian')]
+   assert list_of_ships == ['Guardian'] or list_of_ships == ['Guardian1', 'Guardian2']
