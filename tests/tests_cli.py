c6536f48d9b62bdf714d012077aee490fa519c60
Andrey Smelter <andrey.smelter@gmail.com>
2019-01-29 23:51:34 -0500

    Tests: add ship move command.

--- tests/tests_cli.py
+++ tests/tests_cli.py
@@ -21,3 +21,14 @@ def test_ship_new_cmd(impl, interpreter, interpreter_options, cli_options):
    result = subprocess.check_output(cmd, shell=False, stderr=subprocess.STDOUT)
    list_of_ships = [word for word in result.decode('utf-8').split() if word.startswith('Guardian')]
    assert list_of_ships == ['Guardian'] or list_of_ships == ['Guardian1', 'Guardian2']
+
+
+@pytest.mark.parametrize('interpreter, interpreter_options, cli_options', [
+    ('python', '-m', 'ship Guardian move 10 20')
+])
+def test_ship_move_cmd(impl, interpreter, interpreter_options, cli_options):
+   cmd = [interpreter, interpreter_options, impl]
+   cmd.extend(cli_options.split())
+   result = subprocess.check_output(cmd, shell=False, stderr=subprocess.STDOUT)
+   outcomes = {'Moving ship Guardian to [10,20] with speed 10 KN'}
+   assert result.decode('utf-8').strip() in outcomes
