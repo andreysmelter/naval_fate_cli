0fe16cc04f121d91059814a29c534f7dd2809467
Andrey Smelter <andrey.smelter@gmail.com>
2019-02-07 17:56:38 -0500

    Tests: added tests for optionional arg.

--- tests/tests_cli.py
+++ tests/tests_cli.py
@@ -48,11 +48,14 @@ def test_ship_shoot_cmd(impl, interpreter, interpreter_options, cli_options):
 
 
 @pytest.mark.parametrize('interpreter, interpreter_options, cli_options', [
-    ('python', '-m', 'mine set 10 20')
+    ('python', '-m', 'mine set 10 20'),
+    ('python', '-m', 'mine set 10 20 --drifting'),
+    ('python', '-m', 'mine set 10 20 --moored')
+
 ])
 def test_ship_shoot_cmd(impl, interpreter, interpreter_options, cli_options):
     cmd = [interpreter, interpreter_options, impl]
     cmd.extend(cli_options.split())
     result = subprocess.check_output(cmd, shell=False, stderr=subprocess.STDOUT)
-    outcomes = {'Set drifting mine at [10.0,20.0]'}
+    outcomes = {'Set drifting mine at [10.0,20.0]', 'Set moored mine at [10.0,20.0]'}
     assert result.decode('utf-8').strip() in outcomes
