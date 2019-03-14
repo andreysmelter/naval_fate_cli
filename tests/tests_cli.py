e79bed5a8382c568cbbde2eeb9a9ca417591f7ab
Andrey Smelter <andrey.smelter@gmail.com>
2019-03-13 23:34:47 -0400

    Tests: try to combine all tests into combined parametrized function.

--- tests/tests_cli.py
+++ tests/tests_cli.py
@@ -65,3 +65,20 @@ def test_ship_shoot_cmd(impl, interpreter, interpreter_options, cli_options, tes
                 '3': 'Removed drifting mine at [10.0,20.0]',
                 '4': 'Removed moored mine at [10.0,20.0]'}
     assert result.decode('utf-8').strip() == outcomes[test_case]
+
+
+@pytest.mark.parametrize('interpreter, interpreter_options, cli_options, test_case', [
+    ('python', '-m', '--version', '1'),
+    ('python', '-m', 'ship new Guardian', '2'),
+    ('python', '-m', 'ship new Guardian1 Guardian2', '3')
+])
+def test_combined_cmd(impl, interpreter, interpreter_options, cli_options, test_case):
+    cmd = [interpreter, interpreter_options, impl]
+    cmd.extend(cli_options.split())
+    result = subprocess.check_output(cmd, shell=False, stderr=subprocess.STDOUT)
+    outcomes = {
+        '1': '1.0.0',
+        '2': 'Created ship Guardian',
+        '3': 'Created ship Guardian1\nCreated ship Guardian2'
+    }
+    assert result.decode('utf-8').strip() == outcomes[test_case]
