40428c50f2c482fa1108452afe85c62b25baef30
Andrey Smelter <andrey.smelter@gmail.com>
2019-04-01 23:13:21 -0400

    Tests: added teste for mine set cmd.

--- tests/tests_cli.py
+++ tests/tests_cli.py
@@ -56,7 +56,7 @@ def test_ship_shoot_cmd(impl, interpreter, interpreter_options, cli_options, tes
     ('python', '-m', 'mine remove 10 20 --drifting', '3'),
     ('python', '-m', 'mine remove 10 20 --moored', '4')
 ])
-def test_ship_shoot_cmd(impl, interpreter, interpreter_options, cli_options, test_case):
+def test_mine_set_remove_cmd(impl, interpreter, interpreter_options, cli_options, test_case):
     cmd = [interpreter, interpreter_options, impl]
     cmd.extend(cli_options.split())
     result = subprocess.check_output(cmd, shell=False, stderr=subprocess.STDOUT)
@@ -73,7 +73,11 @@ def test_ship_shoot_cmd(impl, interpreter, interpreter_options, cli_options, tes
     ('python', '-m', 'ship new Guardian1 Guardian2', '3'),
     ('python', '-m', 'ship move Guardian 10 20', '4'),
     ('python', '-m', 'ship move Guardian 30 40 --speed=20', '5'),
-    ('python', '-m', 'ship shoot Guardian 5 15', '6')
+    ('python', '-m', 'ship shoot Guardian 5 15', '6'),
+    ('python', '-m', 'mine set 10 20', '7'),
+    ('python', '-m', 'mine set 10 20 --drifting', '7'),
+    ('python', '-m', 'mine set 10 20 --moored', '8')
+
 ])
 def test_combined_cmd(impl, interpreter, interpreter_options, cli_options, test_case):
     cmd = [interpreter, interpreter_options, impl]
@@ -85,6 +89,8 @@ def test_combined_cmd(impl, interpreter, interpreter_options, cli_options, test_
         '3': 'Created ship Guardian1\nCreated ship Guardian2',
         '4': 'Moving ship Guardian to [10.0,20.0] with speed 10.0 KN',
         '5': 'Moving ship Guardian to [30.0,40.0] with speed 20.0 KN',
-        '6': 'Ship Guardian fires to [5.0,15.0]'
+        '6': 'Ship Guardian fires to [5.0,15.0]',
+        '7': 'Set drifting mine at [10.0,20.0]',
+        '8': 'Set moored mine at [10.0,20.0]'
     }
     assert result.decode('utf-8').strip() == outcomes[test_case]
